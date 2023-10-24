2c0ac9ec562a6c8014969fbe4187da497e82372e
Thomas Wright <thomas.wright@caliqon.com>
2023-10-24 08:29:51 +0100

    updated background task runner

--- demo/Exmediator.Demo.ConsoleApp/Services/DemoTaskExecutor.cs
+++ demo/Exmediator.Demo.ConsoleApp/Services/DemoTaskExecutor.cs
@@ -1,26 +1,43 @@
-﻿using Exmediator.Services;
+﻿using System.Threading.Channels;
+using Exmediator.Services;
 
 namespace Exmediator.Demo.ConsoleApp.Services;
 
 public class DemoTaskExecutor : ITaskExecutor
 {
+    private Channel<Task> _channel = Channel.CreateUnbounded<Task>();
+
+    public DemoTaskExecutor()
+    {
+        new TaskFactory().StartNew(async () =>
+        {
+            while (await _channel.Reader.WaitToReadAsync())
+            {
+                while (_channel.Reader.TryRead(out var task))
+                {
+                    await task;
+                }
+            }
+        });
+    }
+    
     public void Execute(Task task, CancellationToken cancellationToken = default)
     {
-        new TaskFactory(cancellationToken).StartNew(() => task, cancellationToken);
+        _channel.Writer.TryWrite(task);
     }
 
     public void Execute<T>(Task<T> task, CancellationToken cancellationToken = default)
     {
-        new TaskFactory(cancellationToken).StartNew(() => task, cancellationToken);
+        _channel.Writer.TryWrite(task);
     }
 
     public void Execute(ValueTask task, CancellationToken cancellationToken = default)
     {
-        new TaskFactory(cancellationToken).StartNew(() => task, cancellationToken);
+        _channel.Writer.TryWrite(task.AsTask());
     }
 
     public void Execute<T>(ValueTask<T> task, CancellationToken cancellationToken = default)
     {
-        new TaskFactory(cancellationToken).StartNew(() => task, cancellationToken);
+        _channel.Writer.TryWrite(task.AsTask());
     }
 }
